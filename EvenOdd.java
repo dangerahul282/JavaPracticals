859c74577e55ecc6eb26772b573500a1a4d3a3db
dangerahul282 <98229524+dangerahul282@users.noreply.github.com>
2023-08-31 16:21:17 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,19 @@
+package com.java.intro;
+public class EvenOdd{
+
+public static void main(String[] args) {
+
+		//Java Program to Check Whether a Number is Even or Odd
+		
+		int a = 15;
+		if(a%2 == 0) {
+			System.out.println("Gievn number is even : " + a);
+		}
+		else {
+			System.out.println("Given number odd : " + a);
+		}
+		
+		
+	}
+}
+        
