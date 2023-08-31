66138041349b61a5dbf44109d184ee2e9a3814ec
dangerahul282 <98229524+dangerahul282@users.noreply.github.com>
2023-08-31 16:22:00 +0530

    Add files via upload

---
+++ PositiveNegative.java
@@ -0,0 +1,25 @@
+package com.java.intro;
+public class PositiveNegative{
+
+public static void main(String[] args) {
+
+//Java Program to Check Whether a Number is Positive or Negative
+	double number = 12.3;
+
+        if (number < 0.0)
+            System.out.println(number + " is a negative number.");
+
+        else if ( number > 0.0)
+            System.out.println(number + " is a positive number.");
+
+        else
+            System.out.println(number + " is 0.");
+		
+ 	}
+}
+		
+		
+		
+		
+		
+          
