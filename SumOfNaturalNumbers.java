48826bdbbbffbdb093f4d613573a107eba6e53c8
dangerahul282 <98229524+dangerahul282@users.noreply.github.com>
2023-09-04 14:09:27 +0530

    Add files via upload

---
+++ SumOfNaturalNumbers.java
@@ -0,0 +1,7 @@
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+        int n = 10;
+        int sum = (n * (n + 1)) / 2;
+        System.out.println("Sum of the first 10 natural numbers is: " + sum);
+    }
+}
